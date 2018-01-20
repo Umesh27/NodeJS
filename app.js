a3fa6d535fe054be0ee541ba7a76d9e3b1703ab0
Umesh <ushisode1@gmail.com>
2018-01-20 12:56:27 +0530

    lesson 11 completes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("starting app.js");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 // fs.appendFile('greetings.txt', 'Hello World!');
 
@@ -9,8 +10,19 @@ const notes = require('./notes.js');
 //   console.log('Unable to write file !');
 // });
 
-var user = os.userInfo();
-// console.log(user);
-// fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
+// var user = os.userInfo();
+// // console.log(user);
+// // fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
+//
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+// var res = notes.addNote();
+// console.log(res);
+//
+// var addition = notes.addTwoNumbers(4, 5);
+// console.log('Result : ', addition);
+
+console.log(_.isString(true));
+console.log(_.isString('Umesh'));
+var filteredArray = _.uniq(['Umesh', 1,2,3,4,'umesh',1,2,3,4,6,7,2,3,4,2]);
+console.log(filteredArray);
--- notes.js
+++ notes.js
@@ -1,1 +1,13 @@
 console.log('Starting notes.js');
+
+// console.log(module);
+module.exports.age = 25;
+module.exports.addNote = () => {
+  console.log('add note function !');
+  return 'New Note.';
+};
+
+module.exports.addTwoNumbers = (a, b) => {
+  c = a + b;
+  return c
+};
