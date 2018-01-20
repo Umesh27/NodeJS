f88434efeb1e5602fe40c6f9375f313ae1313fed
Umesh <ushisode1@gmail.com>
2018-01-20 13:28:12 +0530

    lesson 13 completed

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('all arguments: ', process.argv);
+
+if (command === 'add'){
+  console.log("Adding new Note!");
+} else if (command === "list") {
+  console.log("Listing all notes");
+} else if (command === "read") {
+  console.log("Reading all notes");
+} else if (command === "remove") {
+  console.log("Removing all notes");
+} else {
+  console.log("command not recongnized");
+}
